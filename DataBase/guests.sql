2280812f52f908b6fcbe12cb7947f5a7686997cc
VitaliyBelous <vitaly87belous@gmail.com>
2015-02-02 16:46:15 +0200

    add DataBase

---
+++ DataBase/guests.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 02, 2015 at 04:42 PM
+-- Server version: 5.5.41-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `guests`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fos_user_group`
+--
+
+CREATE TABLE IF NOT EXISTS `fos_user_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_583D1F3E5E237E06` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fos_user_user`
+--
+
+CREATE TABLE IF NOT EXISTS `fos_user_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `locked` tinyint(1) NOT NULL,
+  `expired` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL,
+  `confirmation_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  `credentials_expired` tinyint(1) NOT NULL,
+  `credentials_expire_at` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `date_of_birth` datetime DEFAULT NULL,
+  `firstname` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastname` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `website` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `biography` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gender` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `locale` varchar(8) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `timezone` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `facebook_uid` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `facebook_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `facebook_data` longtext COLLATE utf8_unicode_ci COMMENT '(DC2Type:json)',
+  `twitter_uid` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `twitter_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `twitter_data` longtext COLLATE utf8_unicode_ci COMMENT '(DC2Type:json)',
+  `gplus_uid` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gplus_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gplus_data` longtext COLLATE utf8_unicode_ci COMMENT '(DC2Type:json)',
+  `token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `two_step_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_C560D76192FC23A8` (`username_canonical`),
+  UNIQUE KEY `UNIQ_C560D761A0D96FBF` (`email_canonical`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `fos_user_user`
+--
+
+INSERT INTO `fos_user_user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `locked`, `expired`, `expires_at`, `confirmation_token`, `password_requested_at`, `roles`, `credentials_expired`, `credentials_expire_at`, `created_at`, `updated_at`, `date_of_birth`, `firstname`, `lastname`, `website`, `biography`, `gender`, `locale`, `timezone`, `phone`, `facebook_uid`, `facebook_name`, `facebook_data`, `twitter_uid`, `twitter_name`, `twitter_data`, `gplus_uid`, `gplus_name`, `gplus_data`, `token`, `two_step_code`) VALUES
+(4, 'adminuser', 'adminuser', 'metan6@rambler.ru', 'metan6@rambler.ru', 1, 'md71gc27pj40s0c4gok88g0oso4gcw0', 'KBJf7991anUqUQbww78hd8kPqQGqxA5sZt+J8nZgyTUuoKT4h0Wl2r573Rwi+sAjHR9YWfPcD+8M0al3LtvdSQ==', '2015-02-01 22:01:16', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:16:"ROLE_SUPER_ADMIN";}', 0, NULL, '2015-01-30 17:00:08', '2015-02-01 22:01:16', NULL, NULL, NULL, NULL, NULL, 'u', NULL, NULL, NULL, NULL, NULL, 'null', NULL, NULL, 'null', NULL, NULL, 'null', NULL, NULL),
+(5, 'Vitalik', 'vitalik', 'vitaly87belous@gmail.com', 'vitaly87belous@gmail.com', 1, '4jfeff7t9hk448w8o8k0804ocgoos8w', 'MJ5ATIt6oOxYRD0DbLn/CZSBb2xSszgLJgTLsKDuaUFgLcSx32W3eHORuU54HQ5QgLNpSGX8BBjpg/8aIVMyMg==', NULL, 0, 0, NULL, NULL, NULL, 'a:0:{}', 0, NULL, '2015-01-30 17:43:44', '2015-01-30 17:44:13', NULL, 'Vitalik', 'Belous', 'http://guestbook.com', NULL, 'm', 'uk_UA', 'Europe/Kiev', '0933759264', NULL, NULL, 'null', NULL, NULL, 'null', NULL, NULL, 'null', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fos_user_user_group`
+--
+
+CREATE TABLE IF NOT EXISTS `fos_user_user_group` (
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`group_id`),
+  KEY `IDX_B3C77447A76ED395` (`user_id`),
+  KEY `IDX_B3C77447FE54D947` (`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Task`
+--
+
+CREATE TABLE IF NOT EXISTS `Task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` text NOT NULL,
+  `dueDate` datetime NOT NULL,
+  `site` text NOT NULL,
+  `comment` text NOT NULL,
+  `point` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=176 ;
+
+--
+-- Dumping data for table `Task`
+--
+
+INSERT INTO `Task` (`id`, `author`, `dueDate`, `site`, `comment`, `point`) VALUES
+(169, 'Vitalik', '2015-02-02 16:23:50', 'http://guestbook.com/app_dev.php', 'My comment', '8'),
+(170, 'Symfony', '2015-02-02 16:24:51', 'http://symfony2.com/', 'Congratulations! You have successfully installed a new Symfony application.', '5'),
+(171, 'Vasia Pupkin', '2015-02-02 16:28:45', 'books.google.com.ua', 'The leading PHP framework to create websites and web applications. Built on top of the Symfony Components.', '10');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `fos_user_user_group`
+--
+ALTER TABLE `fos_user_user_group`
+  ADD CONSTRAINT `FK_B3C77447A76ED395` FOREIGN KEY (`user_id`) REFERENCES `fos_user_user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_B3C77447FE54D947` FOREIGN KEY (`group_id`) REFERENCES `fos_user_group` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
